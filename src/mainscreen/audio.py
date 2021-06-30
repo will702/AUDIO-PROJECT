5b75c484d5264bdc8f5b1c4643242553373f392d
will702 <67593974+will702@users.noreply.github.com>
2021-06-30 10:32:03 +0700

    Make Audio Class

---
+++ src/mainscreen/audio.py
@@ -0,0 +1,65 @@
+class Player(object):
+    from jnius import autoclass
+
+    MediaPlayer = autoclass('android.media.MediaPlayer')
+    AudioManager = autoclass('android.media.AudioManager')
+    mPlayer = MediaPlayer()
+
+    def raw(self):
+        return self.mPlayer
+
+    def play(self, content):
+        self.content = str(content)
+        try:
+            self.mPlayer.stop()
+            self.mPlayer.reset()
+        except:
+            pass
+        self.mPlayer.setDataSource(self.content)
+        self.mPlayer.prepare()
+        self.mPlayer.start()
+
+    def pause(self):
+        self.mPlayer.pause()
+
+    def resume(self):
+        self.mPlayer.start()
+
+    def stop(self):
+        self.mPlayer.stop()
+
+    def stream(self, content):
+        self.content = str(content)
+        self.mPlayer.setAudioStreamType(self.AudioManager.STREAM_MUSIC)
+        try:
+            self.mPlayer.stop()
+            self.mPlayer.reset()
+        except:
+            pass
+        self.mPlayer.setDataSource(self.content)
+        self.mPlayer.prepare()
+        self.mPlayer.start()
+
+    def get_duration(self):
+        if self.content:
+            return self.mPlayer.getDuration()
+
+    def current_position(self):
+        if self.content:
+            return self.mPlayer.getCurrentPosition()
+
+    def seek(self, value):
+        try:
+            self.mPlayer.seekTo(int(value) * 1000)
+        except:
+            pass
+
+    def do_loop(self, loop=False):
+        if not loop:
+            self.mPlayer.setLooping(False)
+        else:
+            self.mPlayer.setLooping(True)
+
+    def is_playing(self):
+        return self.mPlayer.isPlaying
+player = Player()
--- src/service.py
+++ src/service.py
@@ -4,71 +4,7 @@ from time import  sleep
 from oscpy.server import OSCThreadServer
 from oscpy.client import OSCClient
 from kivy.utils import platform
-class MediaPlayer(object):
-    from jnius import autoclass
-
-    MediaPlayer = autoclass('android.media.MediaPlayer')
-    AudioManager = autoclass('android.media.AudioManager')
-    mPlayer = MediaPlayer()
-
-    def raw(self):
-        return self.mPlayer
-
-    def play(self, content):
-        self.content = str(content)
-        try:
-            self.mPlayer.stop()
-            self.mPlayer.reset()
-        except:
-            pass
-        self.mPlayer.setDataSource(self.content)
-        self.mPlayer.prepare()
-        self.mPlayer.start()
-
-    def pause(self):
-        self.mPlayer.pause()
-
-    def resume(self):
-        self.mPlayer.start()
-
-    def stop(self):
-        self.mPlayer.stop()
-
-    def stream(self, content):
-        self.content = str(content)
-        self.mPlayer.setAudioStreamType(AudioManager.STREAM_MUSIC)
-        try:
-            self.mPlayer.stop()
-            self.mPlayer.reset()
-        except:
-            pass
-        self.mPlayer.setDataSource(self.content)
-        self.mPlayer.prepare()
-        self.mPlayer.start()
-
-    def get_duration(self):
-        if self.content:
-            return self.mPlayer.getDuration()
-
-    def current_position(self):
-        if self.content:
-            return self.mPlayer.getCurrentPosition()
-
-    def seek(self, value):
-        try:
-            self.mPlayer.seekTo(int(value) * 1000)
-        except:
-            pass
-
-    def do_loop(self, loop=False):
-        if not loop:
-            self.mPlayer.setLooping(False)
-        else:
-            self.mPlayer.setLooping(True)
-
-    def is_playing(self):
-        return self.mPlayer.isPlaying
-
+from mainscreen.audio import player
 
 class Service(object):
     SERVER = OSCThreadServer()
@@ -104,26 +40,27 @@ class Service(object):
 
         if platform == 'android':
             if self.filename != '':
-                from jnius import autoclass
+                player.play(self.filename)
 
-                MediaPlayer = autoclass('android.media.MediaPlayer')
-                AudioManager = autoclass('android.media.AudioManager')
-                mPlayer = MediaPlayer()
-                mPlayer.setDataSource(self.filename)
-
-                mPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION)
-                mPlayer.prepare()
-
-                mPlayer.start()
-                sleep(mPlayer.getDuration())
-                mPlayer.stop()
+                # from jnius import autoclass
+                #
+                # MediaPlayer = autoclass('android.media.MediaPlayer')
+                # AudioManager = autoclass('android.media.AudioManager')
+                # mPlayer = MediaPlayer()
+                # mPlayer.setDataSource(self.filename)
+                #
+                # mPlayer.setAudioStreamType(AudioManager.STREAM_NOTIFICATION)
+                # mPlayer.prepare()
+                #
+                # mPlayer.start()
+                # sleep(mPlayer.getDuration())
+                # mPlayer.stop()
 
 
 
 
     def send_date(self):
 
-        'send date to the application'
 
 
 
